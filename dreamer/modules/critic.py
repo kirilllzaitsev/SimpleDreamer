ae99b93bee33c91fa97b93253d5873b3df7f8967
kirilllzaitsev <kzaitsev88@gmail.com>
2024-05-24 08:27:22 +0200

    tweak critic.py and model.py

--- dreamer/modules/critic.py
+++ dreamer/modules/critic.py
@@ -4,17 +4,21 @@ from dreamer.utils.utils import build_network, create_normal_dist, horizontal_fo
 
 
 class Critic(nn.Module):
-    def __init__(self, config):
+    def __init__(
+        self, stochastic_size, deterministic_size, hidden_size, num_layers, activation
+    ):
         super().__init__()
-        self.config = config.parameters.dreamer.agent.critic
-        self.stochastic_size = config.parameters.dreamer.stochastic_size
-        self.deterministic_size = config.parameters.dreamer.deterministic_size
+        self.stochastic_size = stochastic_size
+        self.deterministic_size = deterministic_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.activation = activation
 
         self.network = build_network(
             self.stochastic_size + self.deterministic_size,
-            self.config.hidden_size,
-            self.config.num_layers,
-            self.config.activation,
+            self.hidden_size,
+            self.num_layers,
+            self.activation,
             1,
         )
 
