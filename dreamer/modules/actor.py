d7e738ae38852f4fdfe4e8974e00b33a3af72b79
kirilllzaitsev <kzaitsev88@gmail.com>
2024-05-24 08:26:54 +0200

    tweak actor.py

--- dreamer/modules/actor.py
+++ dreamer/modules/actor.py
@@ -2,26 +2,44 @@ import torch
 import torch.nn as nn
 from torch.distributions import TanhTransform
 
-from dreamer.utils.utils import create_normal_dist, build_network
+from dreamer.utils.utils import build_network, create_normal_dist
 
 
 class Actor(nn.Module):
-    def __init__(self, discrete_action_bool, action_size, config):
+    def __init__(
+        self,
+        discrete_action_bool,
+        action_size,
+        stochastic_size,
+        deterministic_size,
+        hidden_size,
+        num_layers,
+        activation,
+        mean_scale,
+        init_std,
+        min_std,
+    ):
         super().__init__()
-        self.config = config.parameters.dreamer.agent.actor
         self.discrete_action_bool = discrete_action_bool
-        self.stochastic_size = config.parameters.dreamer.stochastic_size
-        self.deterministic_size = config.parameters.dreamer.deterministic_size
+        self.stochastic_size = stochastic_size
+        self.deterministic_size = deterministic_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.activation = activation
+        self.mean_scale = mean_scale
+        self.init_std = init_std
+        self.min_std = min_std
 
         action_size = action_size if discrete_action_bool else 2 * action_size
 
         self.network = build_network(
             self.stochastic_size + self.deterministic_size,
-            self.config.hidden_size,
-            self.config.num_layers,
-            self.config.activation,
+            self.hidden_size,
+            self.num_layers,
+            self.activation,
             action_size,
         )
+        self.std = torch.tensor(0.0)
 
     def forward(self, posterior, deterministic):
         x = torch.cat((posterior, deterministic), -1)
@@ -30,13 +48,15 @@ class Actor(nn.Module):
             dist = torch.distributions.OneHotCategorical(logits=x)
             action = dist.sample() + dist.probs - dist.probs.detach()
         else:
-            dist = create_normal_dist(
+            dist, std = create_normal_dist(
                 x,
-                mean_scale=self.config.mean_scale,
-                init_std=self.config.init_std,
-                min_std=self.config.min_std,
+                mean_scale=self.mean_scale,
+                init_std=self.init_std,
+                min_std=self.min_std,
                 activation=torch.tanh,
+                return_std=True,
             )
+            self.std = std
             dist = torch.distributions.TransformedDistribution(dist, TanhTransform())
             action = torch.distributions.Independent(dist, 1).rsample()
         return action
